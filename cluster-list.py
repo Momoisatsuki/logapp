6a44c94d06e2546c008462b77607d2b171b1bb23
Momoisatsuki <neilyolol880929@gmail.com>
2020-04-29 07:36:14 +0800

    new py

---
+++ cluster-list.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+import boto3
+import json
+
+inventory = {}
+
+
+client = boto3.client('ec2', region_name="us-east-1")
+reservations = client.describe_instances()['Reservations']
+
+#reservations[0]['Instances']['Tags']
+for reservation in reservations:
+    for instance in reservation['Instances']:
+        for tag in instance['Tags']:
+            if tag['Key'] == "TNservice":
+                if tag["Value"] == "Automotive":
+                    for tag in instance['Tags']:
+                        if tag['Key'] == "Name":
+                            cluster = tag['Value'].split('-')[1]
+                            servername = tag['Value']
+                            if servername in inventory:
+                                inventory[cluster].append(servername)
+                            else:
+                                inventory[cluster] = [cluster]
+
+print(json.dumps(inventory))
