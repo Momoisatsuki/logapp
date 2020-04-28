c8fca146ea86ee7ab102c2f122e9ec18866b4412
Momoisatsuki <neilyolol880929@gmail.com>
2020-04-29 07:52:24 +0800

    new py

--- cluster-list.py
+++ cluster-list.py
@@ -4,24 +4,26 @@ import json
 
 inventory = {}
 
+def get_cluster(region):
+    client = boto3.client('ec2', region_name="us-east-1")
+    reservations = client.describe_instances()['Reservations']
+    for reservation in reservations:
+        for instance in reservation['Instances']:
+            for tag in instance['Tags']:
+                if tag['Key'] == "TNService":
+                    if tag["Value"] == "Automotive":
+                        for tag in instance['Tags']:
+                            if tag['Key'] == "Name":
+                                cluster = tag['Value'].split('-')[1]
+                                servername = tag['Value']
+                                if cluster in inventory:
+                                    inventory[cluster].append(servername)
+                                else:
+                                    inventory[cluster] = [servername]
 
-client = boto3.client('ec2', region_name="us-east-1")
-reservations = client.describe_instances()['Reservations']
 
-#reservations[0]['Instances']['Tags']
-for reservation in reservations:
-    for instance in reservation['Instances']:
-        for tag in instance['Tags']:
-            if tag['Key'] == "TNService":
-                if tag["Value"] == "Automotive":
-                    for tag in instance['Tags']:
-                        if tag['Key'] == "Name":
-                            cluster = tag['Value'].split('-')[1]
-                            servername = tag['Value']
-                            #print(cluster)
-                            #print(servername)
-                            if servername in inventory:
-                                inventory[cluster].append(servername)
-                            else:
-                                inventory[cluster] = [cluster]
+if __name__ == "__main__":
+    for region in ['us-east-1','us-west-2','ap-northeast-2','eu-central-1']:
+        get_cluster(region)
+
 print(inventory)
